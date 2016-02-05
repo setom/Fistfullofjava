84b2ee8847055332456237859bfde9df362f9bf6
Jonah <Jonah@10.16.26.144>
2016-02-04 19:02:43 -0800

    addded stuff

--- TCSS 360/Easter Egg/src/Developers/Jonah.java
+++ TCSS 360/Easter Egg/src/Developers/Jonah.java
@@ -1,5 +1,12 @@
+
 package Developers;
 
+/**
+ * This class represents the Developer, Jonah.
+ * 
+ * @author Jonah Howard
+ * @version 4 Feb 2016
+ */
 public class Jonah {
 	public void printJonah() {
 		System.out.println("My name is Jonah. End of conversation.");
