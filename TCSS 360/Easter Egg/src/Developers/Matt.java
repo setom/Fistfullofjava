4bab533b850d16fc2b9755f449158a3cb34c4cf8
Jonah <Jonah@10.16.26.144>
2016-02-04 18:49:48 -0800

    Made some fun changes

--- TCSS 360/Easter Egg/src/Developers/Matt.java
+++ TCSS 360/Easter Egg/src/Developers/Matt.java
@@ -2,6 +2,6 @@ package Developers;
 
 public class Matt {
 	public void printMatt() {
-		System.out.println("My Name is Matt. I'm better than Jonah");
+		System.out.println("My Name is Matt. I'm NOT better than Jonah");
 	}
 }
