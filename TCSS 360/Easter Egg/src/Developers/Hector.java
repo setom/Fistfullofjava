c89381b5ca68e66e62605e45d854ccbd79b0f38f
Hector Diaz <hectord@uw.edu>
2016-02-04 19:05:26 -0800

    Prints out my name

--- TCSS 360/Easter Egg/src/Developers/Hector.java
+++ TCSS 360/Easter Egg/src/Developers/Hector.java
@@ -1,12 +1,14 @@
-/**
- * 
- */
+
 package Developers;
 
 /**
- * @author Hector
- *
+ * @author Hector Diaz Contreras
+ * @version 1.0 (02/04/2016)
  */
 public class Hector {
-
+	
+	/** Prints out Hector Diaz Contreras. */
+	public void printHecor() {
+		System.out.println("Hector Diaz Contreras");
+	}
 }
