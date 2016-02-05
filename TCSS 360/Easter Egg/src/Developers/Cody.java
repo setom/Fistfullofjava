a107322d04249c8c73c01ece73b5f28da6cddeaa
Cody Cates <codyc5@uw.edu>
2016-02-04 18:47:58 -0800

    Changed some print text.

--- TCSS 360/Easter Egg/src/Developers/Cody.java
+++ TCSS 360/Easter Egg/src/Developers/Cody.java
@@ -1,7 +1,12 @@
 package Developers;
 
+/**
+ * 
+ * @author Cody Cates
+ * Easter egg. Stuff. Testing if everyone can connect to github.
+ */
 public class Cody {
 	public void printCody() {
-		System.out.println();
+		System.out.println("Hey. I'm Cody Cates. How's it going? \n");
 	}
 }
