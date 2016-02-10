233015549398276eab21d52b4637f506a1119409
Jonah <Jonah@10.16.26.144>
2016-02-10 12:02:49 -0800

    fixed stuff

--- TCSS 360/Easter Egg/src/Developers/Jonah.java
+++ TCSS 360/Easter Egg/src/Developers/Jonah.java
@@ -9,6 +9,6 @@ package Developers;
  */
 public class Jonah {
 	public void printJonah() {
-		System.out.println("My name is Jonah. End of conversation.");
+		System.out.println("My name is Jonah.");
 	}
 }
